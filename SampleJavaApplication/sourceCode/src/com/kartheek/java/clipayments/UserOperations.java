89175664d721b4b0b73daa985c2f6da12bb9496e
devilhash <saikartheek030@gmail.com>
2024-02-07 12:18:15 +0530

    create register functionality

---
+++ SampleJavaApplication/sourceCode/src/com/kartheek/java/clipayments/UserOperations.java
@@ -0,0 +1,28 @@
+package com.kartheek.java.clipayments;
+import com.kartheek.java.clipayments.entity.*;
+
+public class UserOperations {
+	public User doUserRegistration(String fName, String lName, String password, long phNum, String dob,String addr) {
+		User u = new User();
+		u.setFirstName(fName);
+		u.setLastName(lName);
+		u.setPhoneNum(phNum);
+		u.setDateOfBirth(dob);
+		u.setCommunicationAddr(addr);
+		u.setPassword(password);
+		
+		u.setUserId(1234);
+		return u;
+	}
+	
+	public void printUserList(User[] users){
+		for(int i=0;i<users.length;i++) {
+			if(users[i] != null) {
+				System.out.println("User Details of "+ users[i].getFirstName());
+				System.out.println(users[i]);
+			}
+			
+		}
+	}
+
+}
